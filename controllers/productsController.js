715836567ff6fda6a009f7a87e9cf687190c4691
rogeriogomes2009 <rogerio.gomes2009@gmail.com>
2022-03-08 17:31:27 -0300

    update restAPI

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,44 +1,53 @@
+const db = require('../db')
+const Product = require('../models/product')(db)
 
-const remove = (req, res) => {
+const remove = async (req, res) => {
+  await Product.remove(req.params.id)
   res.send({
-    success: true,
-    data: 'id' + req.params.id
+    success: true
   })
 }
 
-const patch = (req, res) => {
-  console.log(req.body)
+const patch = async (req, res) => {
+  const oldProduct = await Product.findById(req.params.id)
+  if(req.body.product){
+    oldProduct.product = req.body.product
+  }
+  if(req.body.price){
+    oldProduct.price = req.body.price
+  }
+  await Product.update(req.params.id, [oldProduct.product, oldProduct.price])
+  res.send({
+    success: true
+  })
+}
+
+const put = async (req, res) => {
+  const { product, price } = req.body
+  await Product.update(req.params.id, [product, price])
+  res.send({
+    success: true
+  })
+}
+
+const create = async (req, res) => {
+const { product, price } = req.body
+  await Product.create ([product, price])
   res.send({
     success: true,
     data: req.body
   })
 }
 
-const put = (req, res) => {
-  console.log(req.body)
-  res.send({
-    success: true,
-    data: req.body
-  })
+const getId = async (req, res) => {
+  const product = await Product.findById(req.params.id)
+  res.send(product)
 }
 
-const create = (req, res) => {
-  console.log(req.body)
+const get = async (req, res) => {
+  const products = await Product.findAll()
   res.send({
-    success: true,
-    data: req.body
-  })
-}
-
-const getId = (req, res) => {
-  res.send({
-    name: 'Product 1'
-  })
-}
-
-const get = (req, res) => {
-  res.send({
-    products:['ALL PRODUCTS']
+    products
   })
 }
 
