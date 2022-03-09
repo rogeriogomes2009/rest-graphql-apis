e8233ab16a1e74e0be65da9204d390fda5183578
rogeriogomes2009 <rogerio.gomes2009@gmail.com>
2022-03-09 13:45:33 -0300

    Update 090322

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,4 +1,4 @@
-const { getAllProducts, createProduct} = require ('./products')
+const { getAllProducts, createProduct, deleteProduct, updateProduct, createImageOnProduct, deleteImageOnProduct} = require ('./products')
 
 const resolvers = {
   //QUERY ==> Busca dados
@@ -7,7 +7,11 @@ const resolvers = {
   },
   //MUTATION ==> Altera, inclui, exclui dados
   Mutation: {
-    createProduct
+    createProduct,
+    deleteProduct,
+    updateProduct,
+    createImageOnProduct,
+    deleteImageOnProduct
 }
 }
 module.exports = resolvers
--- graphql/resolvers/products.js
+++ graphql/resolvers/products.js
@@ -1,13 +1,73 @@
-const getAllProducts = () => [{ id: '1', name:'All Products'}]
+const db = require('../../db')
+const Product = require('../../models/product')(db)
+const { ApolloError } = require('apollo-server-express')
 
-const createProduct = (context,{input}) => {
-  const { id, name} = input
-  console.log(id, name)
+const getAllProducts = async (context, {filter} ) => {
+  let products = null
+  if(filter && filter.categoryId){
+    products = await Product.findAllByCategory(filter.categoryId)
+  }else{
+    products = await Product.findAll()
+  }
+  return products
+}
+
+const createProduct = async (context,{input}) => {
+  const { product, price } = input
+  await Product.create ([product, price])
   return{
-    id, name
+    product, price
   }
 }
+
+const createImageOnProduct = async (context,{productId, input}) => {
+  const { description, url } = input
+    await Product.addImage (productId, [description, url])
+      return {
+        description,
+        url
+      }
+  }
+
+
+const deleteProduct = async (context,{id}) => {
+  await Product.remove(id)
+  return true
+}
+
+const updateProduct = async (context,{id, input}) => {
+  const oldProduct = await Product.findById(id)
+  if(!oldProduct){
+   throw new ApolloError ('Product not found!')
+  }
+  if(input.product){
+    oldProduct.product = input.product
+  }
+  if(input.price){
+    oldProduct.price = input.price
+  }
+  await Product.update(id, [oldProduct.product, oldProduct.price])
+  if(input.categories){ //atualizar categories
+    try{
+    await Product.updateCategories(id, input.categories)
+  }catch(err){
+    throw new ApolloError ('Category not found!')
+  }
+} 
+return oldProduct
+}
+const deleteImageOnProduct = async (context,{id, productId}) => {
+  await Product.removeImage (productId, id)
+  return true
+  
+}
+
+
 module.exports = {
   getAllProducts,
-  createProduct
+  createProduct,
+  deleteProduct,
+  updateProduct,
+  createImageOnProduct,
+  deleteImageOnProduct
 }
